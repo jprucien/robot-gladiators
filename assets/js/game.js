a4b03835987121e427e470b11e1d37b6a770cad9
Jean Prucien <jprucien88@gmail.com>
2022-03-15 14:19:50 -0500

    player name loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,6 +80,7 @@ playerInfo.health = Math.max(0, playerInfo.health - damage);
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
+
       // pick new enemy to fight based on the index of the enemyNames array
       var pickedEnemyObj = enemyInfo[i];
 
@@ -163,8 +164,20 @@ break;
 }
 };
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
